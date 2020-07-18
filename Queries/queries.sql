ae797ab30a3a4e1171e4f65e6bad60f9d148465c
Hannah <66184980+HannahEG@users.noreply.github.com>
2020-07-17 22:01:11 -0500

    updated retirement_info table

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -36,10 +36,13 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 --create table for retirement eligible employees
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
+--check table
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
